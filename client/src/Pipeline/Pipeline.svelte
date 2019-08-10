2f4b9e738a0f9d83876818881f6a2c3d8e77c958
auroratide <tf.auroratide@gmail.com>
2019-08-10 10:56:38 -0500

    Extremely basic styling for pipelines

--- client/src/Pipeline/Pipeline.svelte
+++ client/src/Pipeline/Pipeline.svelte
@@ -1,15 +1,25 @@
 <script>
-  import Stage, { Passed } from '../Stage'
+  import Stage, { Passed, InProgress } from '../Stage'
 </script>
 
 <article class="pipeline">
-  <Stage name="Stage 1" status={Passed} />
-  <Stage name="Stage 2" status={Passed} />
-  <Stage name="Stage 3" status={Passed} />
+  <h1>Aurora</h1>
+  <div class="stages">
+    <Stage name="Stage 1" status={Passed} />
+    <Stage name="Stage 2" status={Passed} />
+    <Stage name="Stage 3" status={Passed} />
+    <Stage name="Stage 4" status={InProgress} />
+  </div>
 </article>
 
 <style>
   .pipeline {
-    border: 0.25em solid red;
+    text-align: center;
+  }
+
+  .stages {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 0.5em;
   }
 </style>
--- client/src/Stage/Stage.svelte
+++ client/src/Stage/Stage.svelte
@@ -5,12 +5,31 @@
   export let status = Unknown
 </script>
 
-<section class="stage">
+<section class={`stage ${status}`}>
   <h1>{name}</h1>
 </section>
 
 <style>
   .stage {
-    border: 0.25em solid black;
+    border: 0.25em solid;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .passed {
+    border-color: green;
+  }
+
+  .failed {
+    border-color: red;
+  }
+
+  .in-progress {
+    border-color: yellow;
+  }
+
+  .pending {
+    border-color: grey;
   }
 </style>
