07c3fd72c79ecf34e93dfea5610da3595aff7fdd
auroratide <tf.auroratide@gmail.com>
2019-08-11 17:06:34 -0500

    Fix linting problems

--- server/src/api/api.example.ts
+++ server/src/api/api.example.ts
@@ -11,7 +11,7 @@ interface MockConfig {
 }
 
 class MockAdapter implements ServiceAdapter<MockConfig> {
-  currentStages(id: string, config: MockConfig): Promise<Stage[]> {
+  public currentStages(id: string, config: MockConfig): Promise<Stage[]> {
     if(id === '1' && config.param === 'value') {
       return Promise.resolve([ {
         name: 'Stage 1',
@@ -26,21 +26,23 @@ class MockAdapter implements ServiceAdapter<MockConfig> {
 describe('api', () => {
   let app: http.Server
 
-  it('uses the service to return a pipeline', async () => { try {
-    api.use('/services/mock-service', pipelines(new MockAdapter()))
-    app = server.listen(3001)
+  it('uses the service to return a pipeline', async () => {
+    try {
+      api.use('/services/mock-service', pipelines(new MockAdapter()))
+      app = server.listen(3001)
 
-    const result = await fetch('http://localhost:3001/api/services/mock-service/pipelines/1?param=value')
-      .then(res => res.json())
+      const result = await fetch('http://localhost:3001/api/services/mock-service/pipelines/1?param=value')
+        .then(res => res.json())
 
-    expect(result).toEqual({
-      name: 'Unknown',
-      stages: [ {
-        name: 'Stage 1',
-        status: Status.Passed
-      } ]
-    })
-  } finally {
-    app.close()
-  }})
+      expect(result).toEqual({
+        name: 'Unknown',
+        stages: [ {
+          name: 'Stage 1',
+          status: Status.Passed
+        } ]
+      })
+    } finally {
+      app.close()
+    }
+  })
 })
